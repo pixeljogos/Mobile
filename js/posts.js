e3b786c463df49ee13d878d626c31bee501d707a
pixeljogos <pixeljogoscontato@gmail.com>
2017-04-14 11:50:29 -0300

    Delete posts.js

--- js/posts.js
+++
@@ -1,8 +0,0 @@
-$('.switch').on('click', function(e) {
-$('#fulljogo').toggleClass('jogo jogofull');
-$('#telapequena').toggleClass('switch switch2');
-});
-$('.switch2').on('click', function(e) {
-$('#fulljogo').toggleClass('jogofull jogo');
-$('#telapequena').toggleClass('switch2 switch');
-});
